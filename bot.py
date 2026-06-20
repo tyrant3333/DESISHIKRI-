ebe6c97e6900e8f047ee7035ba509aea4b6bbe95
lookup57lookup57-bit <lookup57lookup57@gmail.com>
2026-06-21 00:38:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -100,7 +100,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "8550493986:AAG2oasOdP_1Vlc3ojcKd-DyWCPxP_u1a
 ADMIN_ID = json.loads(os.getenv("ADMIN_ID", "[7935621079]"))
 
 # ✅ UPDATED SHOPIFY API URL
-API_BASE_URL = os.getenv("API_BASE_URL", "https://web-production-669be.up.railway.app/shopify")
+API_BASE_URL = os.getenv("API_BASE_URL", "https://autosh.up.railway.app/shopii")
 # ✅ UPDATED RAZORPAY API URL
 RAZORPAY_API_URL = os.getenv("RAZORPAY_API_URL", "https://ravenxkiller.site/rz/r.php")
 # ✅ NEW PAYPAL API URL
